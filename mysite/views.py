9f408bf472bd47a1eb8d047c04d8f50e1501478e
Huweisi <55744817@qq.com>
2024-12-21 17:35:39 +0800

    first commit

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,24 @@
+''' 旧文档
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from mysite.models import Post
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count, post, in enumerate(posts):
+        post_lists.append("NO.{}:".format(str(count)) + str(post) + "<br>")
+        post_lists.append("<small>" + str(post.body) + "</small><br><br>")
+    return HttpResponse(post_lists)
+'''
+
+from django.shortcuts import render
+from mysite.models import Post
+from datetime import datetime
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request,"index.html", locals())
